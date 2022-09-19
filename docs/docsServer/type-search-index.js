d5ec5209d7e0226ed3e21d58f7647617ec29bba4
Andrea Marano <68614754+LuMarans30@users.noreply.github.com>
2022-09-19 14:01:21 +0200

    Add files via upload

--- docs/docsServer/type-search-index.js
+++ docs/docsServer/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Server"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"maranoServer"}];updateSearchResults();
