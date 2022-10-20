50b64e801d4c0cfd3f0c0d191bc7783b2047922c
Andrea Marano <68614754+LuMarans30@users.noreply.github.com>
2022-10-20 07:39:35 +0200

    Add files via upload

---
+++ clientSocket/src/WriteThread.java
@@ -0,0 +1,60 @@
+import java.net.Socket;
+import java.io.DataOutputStream;
+
+public class WriteThread extends Thread {
+  private Socket clientSocket;
+  private DataOutputStream output;
+
+  /**
+   * Messaggio da inviare al server (txtMsg.getText())
+   */
+  private String message;
+
+  public String getExMessage() {
+    return exMessage;
+  }
+
+  private String exMessage;
+
+  /**
+   * Costruttore parametrico
+   * @param clientSocket socket aperto dal client
+   * @param message messaggio da inviare al server
+   */
+  public WriteThread(Socket clientSocket, String message) {
+    this.clientSocket = clientSocket;
+    this.message = message;
+    this.exMessage = "false";
+  }
+
+  /**
+   * Scrittura del messaggio al server
+   */
+  @Override
+  public void start() {
+    try {
+      output = new DataOutputStream(clientSocket.getOutputStream());
+      output.writeUTF(message);
+    } catch (Exception ex) {
+      exMessage = ex.getMessage();
+    }
+
+  }
+
+  /**
+   * Terminazione del thread.
+   * Con chiusura dello stream di output e del socket.
+   */
+  @Override
+  public void interrupt() {
+    try {
+      output.close();
+      clientSocket.close();
+    } catch (Exception ex) {
+      System.err.println(ex.getMessage());
+    }
+
+    super.interrupt();
+  }
+
+}
---
+++ serverSocket/src/ReadThread.java
@@ -0,0 +1,63 @@
+import java.net.Socket;
+
+import java.io.DataInputStream;
+
+public class ReadThread extends Thread {
+  private Socket clientSocket;
+  private DataInputStream input;
+  private String message;
+
+  public String getExMessage() {
+    return exMessage;
+  }
+
+  /**
+   * Se viene lanciata un'eccezzione da start() o interrupt()
+   */
+  private String exMessage;
+
+  public ReadThread(Socket clientSocket){
+    this.clientSocket = clientSocket;
+    this.message = "";
+    this.exMessage = "false";
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  /**
+   * Lettura del messaggio ricevuto dal client
+   */
+  @Override
+  public void start() {
+
+    try {
+      input = new DataInputStream(clientSocket.getInputStream());
+      message = input.readUTF();
+    }catch(Exception ex)
+    {
+      exMessage = ex.getMessage();
+    }
+
+  }
+
+  /**
+   * Terminazione del thread.
+   * Con chiusura dello stream di input e del socket.
+   */
+  @Override
+  public void interrupt() {
+
+    try {
+      input.close();
+      clientSocket.close();
+    } catch (Exception ex) {
+      System.err.println(ex.getMessage());
+    }
+
+    super.interrupt();
+
+  }
+
+}
